d09e3725380e2f0014f47c8d35fd75117fbb7f31
Matt Pillar <matt@pillarcomputing.com>
2016-09-04 20:47:15 -0700

    Solution to the demo problem discussed on the blog

--- demo/demo.py
+++ demo/demo.py
@@ -1,11 +1,14 @@
 import unittest
-import sys
 
 
 class Test(unittest.TestCase):
     test_cases = [
-        [[1, 1], 0],
-        [[3, 1, 2, 4, 3], 1]
+        [[-1, 3, -4, 5, 1, -6, 2, 1], 1],  # or 3, or 7
+        [[1, 0, 1], 1],
+        [[1, -1], -1],
+        [[0, 1, -1], 0],
+        [[], -1],
+        [[1], 0]
     ]
 
     def test(self):
@@ -15,16 +18,18 @@ class Test(unittest.TestCase):
 
 
 def solution(array):
+    if len(array) == 0:
+        return -1
     # Pass through the array once to compute the running sum.
     sums = [array[0]] * len(array)
     for i in range(1, len(array)):
         sums[i] = sums[i-1] + array[i]
-    # Pass through a second time to find the minimum as described in the problem.
-    result = sys.maxint
-    for p in range(1, len(array)):
-        tmp = abs(sums[-1] - 2 * sums[p-1])
-        result = min(result, tmp)
-    return result
+    # Pass through a second time to find the first equilibrium index, if it exists.
+    for p in range(0, len(array)):
+        tmp = sums[-1] - array[p] - 2 * (0 if p == 0 else sums[p-1])
+        if tmp == 0:
+            return p
+    return -1
 
 
 if __name__ == '__main__':
